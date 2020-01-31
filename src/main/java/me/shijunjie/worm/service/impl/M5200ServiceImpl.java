ef3691e9c646caa3d86bbcc958cb8e6b75a6cc2f
shijunjie <me.shijunjie@outlook.com>
2020-01-31 14:47:05 +0800

    添加链接重试机制

--- src/main/java/me/shijunjie/worm/service/impl/M5200ServiceImpl.java
+++ src/main/java/me/shijunjie/worm/service/impl/M5200ServiceImpl.java
@@ -3,6 +3,7 @@ package me.shijunjie.worm.service.impl;
 import me.shijunjie.worm.bean.BookInfo;
 import me.shijunjie.worm.bean.BookSearchRequest;
 import me.shijunjie.worm.service.M5200Service;
+import me.shijunjie.worm.utils.JSoupUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
@@ -25,12 +26,7 @@ public class M5200ServiceImpl implements M5200Service {
     public List<BookInfo> search(BookSearchRequest bookSearchRequest) {
         List<BookInfo> bookInfos = new ArrayList<>();
         String url = "https://m.5200.net/s.php?ie=gbk&s=5256649918672294880&q=" + bookSearchRequest.getBookName();
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(url).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
-        } catch (IOException e) {
-            throw new RuntimeException("连接失败");
-        }
+        Document doc = JSoupUtils.connect(url);
         Elements bookinfos = doc.getElementsByClass("bookinfo");
         bookinfos.forEach(bookinfo -> {
             String bookName = bookinfo.getElementsByTag("a").get(0).text();
@@ -50,9 +46,8 @@ public class M5200ServiceImpl implements M5200Service {
     @Override
     public void getBookContent(String link, BufferedOutputStream buffer) {
         String url = rootUrl + link;
-        Document doc = null;
+        Document doc = JSoupUtils.connect(url);
         try {
-            doc = Jsoup.connect(url).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
             String firstChapterUrl = doc.getElementsByClass("book_last").get(1).getElementsByTag("dd").get(0).getElementsByTag("a").attr("href");
             url = rootUrl + firstChapterUrl;
             getText(url, buffer, link);
@@ -74,22 +69,17 @@ public class M5200ServiceImpl implements M5200Service {
     }
 
     @Override
-    public Map<String,String> getContentHtml(String link) {
-        Map<String,String> resultMap = new HashMap<>();
+    public Map<String, String> getContentHtml(String link) {
+        Map<String, String> resultMap = new HashMap<>();
         String url = rootUrl + "/18_18146/";
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(url).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
-            String contentLink = doc.getElementsByClass("book_last").get(1).getElementsByTag("dd").get(0).getElementsByTag("a").get(0).attr("href");
-            String title = doc.getElementsByTag("title").get(0).text();
-            Map<String, String> contentMap = readContent(contentLink);
-            resultMap.put("title", title);
-            resultMap.put("contentHtml", contentMap.get("contentHtml"));
-            resultMap.put("prevLink", contentMap.get("prevLink"));
-            resultMap.put("nextLink", contentMap.get("nextLink"));
-        } catch (IOException e) {
-            throw new RuntimeException("连接失败：" + e.getMessage());
-        }
+        Document doc = JSoupUtils.connect(url);
+        String contentLink = doc.getElementsByClass("book_last").get(1).getElementsByTag("dd").get(0).getElementsByTag("a").get(0).attr("href");
+        String title = doc.getElementsByTag("title").get(0).text();
+        Map<String, String> contentMap = readContent(contentLink);
+        resultMap.put("title", title);
+        resultMap.put("contentHtml", contentMap.get("contentHtml"));
+        resultMap.put("prevLink", contentMap.get("prevLink"));
+        resultMap.put("nextLink", contentMap.get("nextLink"));
         return resultMap;
     }
 
@@ -97,12 +87,7 @@ public class M5200ServiceImpl implements M5200Service {
     public Map<String, String> readContent(String link) {
         Map<String, String> resultMap = new HashMap<>();
         String url = rootUrl + link;
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(url).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
-        } catch (IOException e) {
-            throw new RuntimeException("连接失败：" + e.getMessage());
-        }
+        Document doc = JSoupUtils.connect(url);
         String innerHtml = doc.getElementById("chaptercontent").html();
         String prevLink = doc.getElementById("pb_prev").attr("href");
         String nextLink = doc.getElementById("pb_next").attr("href");
@@ -114,16 +99,16 @@ public class M5200ServiceImpl implements M5200Service {
 
     private void curPageChapters(String link, String nextLink, List<String> chapters) throws IOException {
         nextLink = readChapters(nextLink, chapters);
-        while(!StringUtils.isEmpty(nextLink) && !link.equals(nextLink)) {
+        while (!StringUtils.isEmpty(nextLink) && !link.equals(nextLink)) {
             nextLink = readChapters(nextLink, chapters);
         }
     }
 
     private String readChapters(String nextLink, List<String> chapters) throws IOException {
         String url = rootUrl + nextLink;
-        Document doc = Jsoup.connect(url).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
+        Document doc = JSoupUtils.connect(url);
         Elements chapterElements = doc.getElementsByClass("book_last").get(1).getElementsByTag("dd");
-        for(int index = 0; index < chapterElements.size(); index++) {
+        for (int index = 0; index < chapterElements.size(); index++) {
             chapters.add(chapterElements.get(index).text());
         }
         nextLink = doc.getElementsByClass("right").get(0).getElementsByTag("a").get(0).attr("href");
@@ -132,13 +117,13 @@ public class M5200ServiceImpl implements M5200Service {
 
     private void getText(String chapterUrl, BufferedOutputStream buffer, String link) throws IOException {
         String nextUrl = writeTxt(chapterUrl, buffer);
-        while(!(rootUrl+link).equals(nextUrl)) {
+        while (!(rootUrl + link).equals(nextUrl)) {
             nextUrl = writeTxt(nextUrl, buffer);
         }
     }
 
     private String writeTxt(String chapterUrl, BufferedOutputStream buffer) throws IOException {
-        Document doc = Jsoup.connect(chapterUrl).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
+        Document doc = JSoupUtils.connect(chapterUrl);
         String nextLink = doc.getElementsByClass("Readpage_down js_page_down").get(0).attr("href");
         String nextUrl = rootUrl + nextLink;
         String innerHtml = doc.getElementsByClass("Readarea ReadAjax_content").get(0).html();
---
+++ src/main/java/me/shijunjie/worm/utils/JSoupUtils.java
@@ -0,0 +1,30 @@
+package me.shijunjie.worm.utils;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class JSoupUtils {
+    public static Document connect(String url) {
+        Document document = null;
+        int retryCnt = 0;
+        while(retryCnt < 3) {
+            try {
+                document = Jsoup.connect(url).header("Sec-Fetch-Mode", "cors").timeout(4000).userAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36").get();
+                return document;
+            } catch (IOException e) {
+                retryCnt ++;
+                if(retryCnt > 2) {
+                    throw new RuntimeException("连接失败：" + e.getMessage());
+                }
+                try {
+                    TimeUnit.MILLISECONDS.sleep(100);
+                } catch (InterruptedException ex) {
+                }
+            }
+        }
+        return document;
+    }
+}
